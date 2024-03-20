248aba32ebc1f4b237ecc0ab78bb32b2df96a727
DrRon <DoctorRon64@users.noreply.github.com>
2024-03-20 01:44:46 +0100

    Project Setup Wave Function Collapse

---
+++ alternativeworlds/Assets/Scripts/Tile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile[] upNeighbours;
+    public Tile[] rightNeighbours;
+    public Tile[] downNeighbours;
+    public Tile[] leftNeighbours;
+}
